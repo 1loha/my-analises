103f99b05bf20269a229d30f6ad198f85516b4b1
Gambino2 <120211034+Gambino2@users.noreply.github.com>
2023-04-19 17:33:44 +0300

    Add files via upload

--- my-analises.py
+++ my-analises.py
@@ -1,105 +1,199 @@
 import datetime
 from tkinter import *
+from tkinter import ttk
 
 # Инициализация окна
-window = Tk()
-
-window.title('Cash App')
-window.geometry('800x500')
-window.minsize(800, 500)
-
-# window.maxsize(1200, 750)
-window.columnconfigure(0, weight=1)
-window.rowconfigure(0, weight=1)
-window.columnconfigure(1, weight=1)
-window.rowconfigure(1, weight=1)
+main_window = Tk()
+main_window.title('Cash App')
+main_window.geometry('1280x720')
+main_window.minsize(800, 600)
+main_window.maxsize(1920, 1080)
+main_window.columnconfigure(0, weight=1)
+main_window.rowconfigure(0, weight=1)
+main_window.columnconfigure(1, weight=1)
+main_window.rowconfigure(1, weight=1)
 
 # Настройки
-graphWidth = 800 #window.winfo_width()
-graphHeight = 180 #window.winfo_height()
+graphWidth = 640
+graphHeight = 240
 graphLineWidth = 2
 
 # Глобальные переменные
 graphValues = []
-valuesNumber = 5
+valuesNumber = 1
+
+buttonsArray = []
+date = "1"
+month_value = "January"
 
 
 # дизайн нижней части окна
 def RenderBottom():
     today = datetime.datetime.today()
-    firstDay = today.day
-    #firstDay = datetime.datetime()
     for i in range(5):
-        dateLabel = Label(tableFrame, text=(today + datetime.timedelta(days=i)).strftime("%d %B"))
-        graphValues.append(Entry(tableFrame))
+        valueLabel = Label(valuesFrame, text=(today + datetime.timedelta(days=i)).strftime("%d %B"))
+        graphValues.append(Entry(valuesFrame))
 
-        dateLabel.grid(row=0, column=i, sticky="w", padx=10, pady=10)
+        valueLabel.grid(row=0, column=i, sticky="w", padx=10, pady=10)
         graphValues[i].grid(row=1, column=i, sticky="e", padx=10, pady=10)
 
-    Button(tableFrame, text="Render Graph", command=RenderGraph).grid()
+    Button(valuesFrame, text="Render Graph", command=RenderGraph).grid()
 
 
 # визуализация графика
 def RenderGraph():
-    canv = Canvas(graphFrame, width=800, height=250, bg="#ADFF2F")
-    canv.place(relx=0, rely=0, anchor="nw")
+    canv = Canvas(graphFrame, width=graphWidth, height=graphHeight, bg="#FFFFFF")
+    canv.place(relx=.5, rely=.5, anchor="center")
 
-    pad_x = 20
-    pad_y = 20
-
-    # ось абсцисс
-    canv.create_line(pad_x, graphHeight, graphWidth, graphHeight, width=graphLineWidth, arrow=LAST)
-    # ось ординат
-    canv.create_line(pad_x, graphHeight, pad_x, 0, width=graphLineWidth, arrow=LAST)
-    # аля данные с таблицы
-    value = [1, 5, 2, 3, 4, 4, 8, 2]
-
-    ordinate = graphHeight
-    ordinate_step = ordinate // len(value)
-    abscissa = graphWidth
-    abscissa_step = abscissa // len(value)
-
-    # отметки растянутые на ось y
-    for i in range(graphHeight, pad_y, -ordinate_step):
-        startLine1 = pad_x - 5
-        startLine2 = pad_x + 5
-        end1, end2 = ordinate, ordinate
-        canv.create_line(startLine1, end1 - i, startLine2, end2 - i, width=1)
-
-    # отметки растянутые на ось x
-    # пока с косяком
-    for i in range(pad_x, graphWidth, abscissa_step):
-        startLine01, startLine02 = pad_x*2, pad_x*2
-        end01 = graphHeight + 5
-        end02 = graphHeight - 5
-        canv.create_line(startLine01 + i, end01, startLine02 + i, end02, width=1)
-
+    # Оси координат
+    canv.create_line(graphWidth / 2, graphHeight, graphWidth / 2, 0, width=graphLineWidth, arrow=LAST)
+    canv.create_line(0, graphHeight / 2, graphWidth, graphHeight / 2, width=graphLineWidth, arrow=LAST)
 
     # Просчет графика
-    # for i in range(valuesNumber-1):
-    #     firstValue = graphValues[i].get()
-    #     secondValue = graphValues[i+1].get()
-    #
-    #     if(firstValue != '' and secondValue!=''):
-    #         canv.create_text(graphWidth / valuesNumber * i, graphHeight / 2 + 20, text="13 April", fill="#45C4B0",
-    #                          font=("Helvectica", "10"))
-    #         canv.create_line(graphWidth / valuesNumber * i, graphHeight/2-int(firstValue), graphWidth / valuesNumber * (i+1),  graphHeight/2-int(secondValue),
-    #                      width=graphLineWidth)
+    for i in range(valuesNumber - 1):
+        firstValue = graphValues[i].get()
+        secondValue = graphValues[i + 1].get()
+
+        if (firstValue != '' and secondValue != ''):
+            canv.create_text(graphWidth / valuesNumber * i, graphHeight / 2 + 20, text="13 April", fill="#45C4B0",
+                             font=("Helvectica", "10"))
+            canv.create_line(graphWidth / valuesNumber * i, graphHeight / 2 - int(firstValue),
+                             graphWidth / valuesNumber * (i + 1), graphHeight / 2 - int(secondValue),
+                             width=graphLineWidth)
+
+
+def click_date_button(value):
+    global date
+    date = str(value)
+    current_date.config(text="Selected date: " + date + " " + month_value)
+
+def check(event):
+    i = 0
+    value = 1
+    day = 1
+    global date
+    for button in buttonsArray:
+        button.grid_forget()
+    # buttonsArray.clear()
+    global month_value
+    month_value = event.widget.get()
+    if month_value in ["January", "March", "May", "July", "August", "October", "December"]:
+        for r in range(3, 6):
+            for col in range(10):
+                value_button = ttk.Button(add_value_window, text=f"{value}", width=23, command=lambda c=day: click_date_button(c))
+                value += 1
+                buttonsArray.append(value_button)
+                value_button.grid(row=r, column=col, rowspan=1, columnspan=1)
+                i = r
+                day += 1
+        value_button = ttk.Button(add_value_window, text=f"{value}", width=23,command=lambda c=day: click_date_button(c))
+        value += 1
+        value_button.grid(row=i + 1, column=0, rowspan=1, columnspan=1)
+        buttonsArray.append(value_button)
+    elif month_value in ["April", "June", "September", "November"]:
+        if int(date) > 30:
+            date = str(30)
+        for r in range(3, 6):
+            for col in range(10):
+                value_button = ttk.Button(add_value_window, text=f"{value}", width=23, command=lambda c=day: click_date_button(c))
+                value += 1
+                buttonsArray.append(value_button)
+                value_button.grid(row=r, column=col, rowspan=1, columnspan=1)
+                day += 1
+    else:
+        if int(date) > 28:
+            date = str(28)
+        for r in range(3, 5):
+            for col in range(10):
+                value_button = ttk.Button(add_value_window, text=f"{value}", width=23, command=lambda c=day: click_date_button(c))
+                value += 1
+                buttonsArray.append(value_button)
+                value_button.grid(row=r, column=col, rowspan=1, columnspan=1)
+                day += 1
+        for r in range(5,6):
+            for col in range(8):
+                value_button = ttk.Button(add_value_window, text=f"{value}", width=23, command=lambda c=day: click_date_button(c))
+                value += 1
+                buttonsArray.append(value_button)
+                value_button.grid(row=r, column=col, rowspan=1, columnspan=1)
+                day += 1
+    current_date.config(text="Selected date: " + date + " " + month_value)
+
+
+
+
+def ClickAddValue():
+    global add_value_window
+    add_value_window = Toplevel()
+    add_value_window.grab_set()
+    add_value_window.title("Input date")
+    add_value_window.geometry("300x300")
+    add_value_window.resizable(False, False)
+    day = 1
+    for c in range(16):
+        add_value_window.columnconfigure(index=c, weight=1)
+    for r in range(16):
+        add_value_window.rowconfigure(index=r, weight=1)
+    label = Label(add_value_window, text="Choose month")
+    label.grid(column=3, row=0, rowspan=1, columnspan=4)
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November", "December"]
+    months_var = StringVar(value=months[0])
+    select_month = ttk.Combobox(add_value_window, textvariable=months_var, values=months, state="readonly",
+                                justify=CENTER,
+                                height=5, width=33)
+    select_month.bind("<<ComboboxSelected>>", lambda e: add_value_window.focus())
+    select_month.grid(column=3, row=1, rowspan=1, columnspan=4)
+    value = 1
+    i = 0
+    for r in range(3, 6):
+        for col in range(10):
+            value_button = ttk.Button(add_value_window, text=f"{value}", width=23, command=lambda c=day: click_date_button(c))
+            value += 1
+            buttonsArray.append(value_button)
+            value_button.grid(row=r, column=col, rowspan=1, columnspan=1)
+            i = r
+            day += 1
+    value_button = ttk.Button(add_value_window, text=f"{value}", width=23, command=lambda c=day: click_date_button(c))
+    value_button.grid(row=i+1, column=0, rowspan=1, columnspan=1)
+    number = date
+    buttonsArray.append(value_button)
+    select_month.bind("<<ComboboxSelected>>", check)
+    confirm_button = ttk.Button(add_value_window, text="Ok", width=33, command= lambda: add_value_window.destroy())
+    confirm_button.grid(row=10, column=4, rowspan=1, columnspan=2)
+    global current_date
+    current_date = Label(add_value_window, text="Selected date: " + number + " " + month_value)
+    current_date.grid(column=2, row=7, rowspan=1, columnspan=6)
+    add_value_window.mainloop()
+    return current_date
+
 
 # рамки, сейчас они просто в оперативке
-#notesFrame = Frame(window, background='#45C4B0')
-graphFrame = Frame(window, background='#ADFF2F')
-tableFrame = Frame(window, background='#1AFFD2')
+notesFrame = Frame(main_window, background='#45C4B0')
+graphFrame = Frame(main_window, background='#FFFFFF')
+valuesFrame = Frame(main_window, background='#DAFDBA')
+
+
+def AddValue():
+    ClickAddValue()
+    value = current_date
+    valueLabel = Label(valuesFrame, text=value)
+    graphValues.append(Entry(valuesFrame))
+
+    valueLabel.grid(row=0, column=6, sticky="w", padx=10, pady=10)
+    graphValues[len(graphValues)].grid(row=1, column=len(graphValues), sticky="e", padx=10, pady=10)
+
 
 # фиксируем их на плоскость с помощью упаковщиков
 # pack()-установка по пикселям
 # grid()-установка на таблице
 # place()-установка по координатам
-#notesFrame.grid(row=0, column=0, sticky="nsew")
-graphFrame.grid(columnspan=2, sticky="nsew")
-tableFrame.grid(columnspan=2, sticky="nsew")
+notesFrame.grid(row=0, column=0, sticky="nsew")
+graphFrame.grid(row=0, column=1, sticky="nsew")
+valuesFrame.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 RenderBottom()
 RenderGraph()
+Button(valuesFrame, text="+", command=AddValue).grid(column=37, row=40)
 
-window.mainloop()
+main_window.mainloop()
